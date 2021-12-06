17bd88f4a3334d41b2ab9d2a2f1f26f3aad40f22
Lisovenko Anton <70dog@mail.ru>
2021-12-06 12:01:57 +0500

    missed the point that I created a test project in a separate outside folder

---
+++ Triton_test_task.Tests/DivecesHandlerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Triton_test_task.Models;
+using Xunit;
+
+namespace Triton_test_task.Tests
+{
+    public class DivecesHandlerTests
+    {
+        private DevicesHandler<Device> devicesHandler;
+       
+        public DivecesHandlerTests()
+        {
+            devicesHandler = new DevicesHandler<Device>();
+        }
+
+        [Fact]
+        public void AddNewDeviceTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void UpdateDeviceTest()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
---
+++ Triton_test_task.Tests/UdpHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Triton_test_task.Models;
+using Xunit;
+
+namespace Triton_test_task.Tests
+{
+    public class UdpHandlerTests
+    {
+        UDPHandler udpHandler;
+
+        public UdpHandlerTests()
+        {
+            udpHandler = new UDPHandler(62006, 62005);
+        }
+
+        [Fact]
+        public void DataRecieveTest()
+        {
+            foreach(byte[] data in udpHandler.Listen())
+            {
+                Assert.True(data.Length > 0);
+                udpHandler.IsListen = false;
+            }
+        }
+
+        [Fact]
+        public void DataSendTest()
+        {
+            throw new NotImplementedException();
+            //Send, get answer and check status
+        }
+
+
+
+    }
+}
